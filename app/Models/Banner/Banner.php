f5c733967e6467b9f9ca620b3d5521b6389b5103
Gandobilis <lashadeveloper@gmail.com>
2023-09-22 19:53:25 +0400

    Banner Polymorphic relationship to images table implemented.

--- app/Models/Banner/Banner.php
+++ app/Models/Banner/Banner.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\Banner;
 
+use App\Models\Image;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Banner extends Model implements TranslatableContract
 {
@@ -14,4 +16,9 @@ class Banner extends Model implements TranslatableContract
 
     public $translatedAttributes = ['title', 'content', 'link'];
     protected $fillable = ['type'];
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
