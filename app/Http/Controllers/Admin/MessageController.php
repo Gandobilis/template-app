b20c6baf55e65b4335d11e186bdba671ef0b0a08
Gandobilis <lashadeveloper@gmail.com>
2023-09-28 20:31:58 +0400

    Admin Message controller destroy method.

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -1,50 +1,63 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Requests\MessageRequest;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class MessageController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the messages.
      */
     public function index()
     {
-        //
+        $messages = Message::all();
+        return response(['messages' => $messages], Response::HTTP_OK);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created message in the database.
      */
-    public function store(Request $request)
+    public function store(MessageRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $message = Message::create($data);
+        return response(['message' => $message], Response::HTTP_CREATED);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified message.
      */
     public function show(Message $message)
     {
-        //
+        return response(['message' => $message], Response::HTTP_OK);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified message in the database.
      */
-    public function update(Request $request, Message $message)
+    public function update(MessageRequest $request, Message $message)
     {
-        //
+        $data = $request->validated();
+
+        $message->update($data);
+        return response(['message' => $message], Response::HTTP_OK);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified message from the database.
      */
-    public function destroy(Message $message)
+    public function destroy(Message $message): Response
     {
-        //
+        $message->delete();
+
+        return response([
+            'message' => 'Message deleted.'
+        ], ResponseAlias::HTTP_OK);
     }
 }
