c6c9a55110bc54c9617a7de24515aa1be4f62dbd
Gandobilis <lashadeveloper@gmail.com>
2023-09-29 11:06:40 +0400

    Public banners controller implemented.

--- app/Http/Controllers/Public/BannerController.php
+++ app/Http/Controllers/Public/BannerController.php
@@ -3,17 +3,11 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\BannerRequest;
 use App\Models\Banner\Banner;
-use App\Services\FileUploadService;
 use Illuminate\Http\Response;
 
 class BannerController extends Controller
 {
-    public function __construct(private readonly FileUploadService $fileUploadService)
-    {
-    }
-
     public function index(): Response
     {
         $banners = Banner::with('image')->get();
@@ -30,31 +24,5 @@ class BannerController extends Controller
         return response([
             'banner' => $banner
         ], 200);
-    }
-
-    public function store(BannerRequest $request): Response
-    {
-        $data = $request->validated();
-        $images = $data['images'] ?? [];
-        unset($data['images']);
-
-        $banner = Banner::create($data);
-
-        $uploadedImages = [];
-
-        foreach ($images as $image) {
-            $uploadedImages[] = $this->fileUploadService->uploadFile($image, 'banners');
-        }
-
-        $banner->images()->createMany(array_map(function ($image) {
-            return ['image' => $image];
-        }, $uploadedImages));
-
-        $banner->load('images');
-
-        return response([
-            'message' => 'Banner created.',
-            'banner' => $banner
-        ], 201);
     }
 }
