b28429d60b4925e317332d14fb76f229d20a96f9
Gandobilis <lashadeveloper@gmail.com>
2023-10-02 16:33:59 +0400

    Message response translations added.

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,8 +1,19 @@
 <?php
 
 return [
-    'create' => 'Message created.',
-    'update' => 'Message updated.',
-    'delete' => 'Message deleted.',
-    'restore' => 'Message restored.'
+    'success' => [
+        'store' => 'Message created successfully.',
+        'update' => 'Message updated successfully.',
+        'destroy' => 'Message deleted successfully.',
+        'restore' => 'Message restored successfully.'
+    ],
+    'error' => [
+        'index' => 'You do not have permission to see messages',
+        'show' => 'You do not have permission to see message',
+        'store' => 'You do not have permission to create messages',
+        'update' => 'You do not have permission to update messages',
+        'destroy' => 'You do not have permission to delete messages',
+        'archived' => 'You do not have permission to see archived messages',
+        'restore' => 'You do not have permission to restore messages'
+    ]
 ];
